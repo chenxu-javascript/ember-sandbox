11ad13e3db8d3e576233eae101cbf111e1a1e890
15215174220a <843313654@qq.com>
2016-09-30 14:27:14 +0800

    测试是否上传成功

---
+++ app/modules/components/demo-ember/component.js
@@ -0,0 +1,8 @@
+import Component from 'ember-component';
+import computed from 'ember-computed';
+import get from 'ember-metal/get';
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+
+});
